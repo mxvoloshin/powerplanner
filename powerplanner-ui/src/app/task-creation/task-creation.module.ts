428941d0401462d1eb724d6e3677ea54f7ed2b5d
Maksym Voloshyn <mx.voloshin@gmail.com>
2018-03-28 22:48:44 +0300

    added task component

---
+++ powerplanner-ui/src/app/task-creation/task-creation.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskCreationComponent } from './task-creation/task-creation.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TaskCreationComponent]
+})
+export class TaskCreationModule { }
